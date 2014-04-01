b7818a2a13331f390155098c19b2ae5b46b72e82
JR <jafierroms@gmail.com>
2014-04-01 11:55:52 -0400

    JR bring up

--- config/nfc_enhanced.mk
+++ config/nfc_enhanced.mk
@@ -1,3 +1,3 @@
 # This device supports JR enhanced NFC
 PRODUCT_COPY_FILES += \
-    vendor/jr/config/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
+    vendor/jr/config/permissions/com.jafierrom.nfc.enhanced.xml:system/etc/permissions/com.jafierrom.nfc.enhanced.xml
