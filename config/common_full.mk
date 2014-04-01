36ade150e7c8828eca183b9724729e9364041080
JR <jafierroms@gmail.com>
2014-04-01 11:36:27 -0400

    JR bring up

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,16 +1,16 @@
-# Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common.mk)
+# Inherit common JR stuff
+$(call inherit-product, vendor/jr/config/common.mk)
 
 # Bring in all video files
 $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
-# Include CM audio files
-include vendor/cm/config/cm_audio.mk
+# Include JR audio files
+include vendor/jr/config/jr_audio.mk
 
-# Include CM LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
+# Include JR LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/jr/overlay/dictionaries
 
-# Optional CM packages
+# Optional JR packages
 PRODUCT_PACKAGES += \
     Galaxy4 \
     HoloSpiralWallpaper \
